59cda7164194e7f6138502d7ddd9f836183e3e59
Tega Lucky-Fiakpa <oghenetegalucky@gmail.com>
2022-11-10 11:40:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let slider = document.querySelector(".pix");
+
+let images = [
+	"apic.jpg",
+	"bpic.jpg",
+	"cpic.jpg",
+	"dpic.jpg",
+	"epic.jpg",
+	"fpic.jpg",
+];
+
+let x = 0; // current image
+
+document.getElementById("prev-btn").onclick = function prev() {
+	if (x <= 0) x = images.length;
+	x--;
+	return addImg();
+};
+
+document.getElementById("next-btn").onclick = function next() {
+	if (x >= images.length - 1) x = -1;
+	x++;
+	return addImg();
+};
+
+function addImg() {
+	return slider.setAttribute("src", "images/" + images[x]);
+}
